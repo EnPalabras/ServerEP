607f49619d2bb7f6e59f6378447c87cea7ad45e9
AgusIglesias72 <enpalabrass@gmail.com>
2023-05-04 01:38:24 -0300

    add some ML endpoints

---
+++ src/MercadoLibre/utils/manageOrders.js
@@ -0,0 +1,20 @@
+import { Prisma } from '@prisma/client'
+import dotenv from 'dotenv'
+import fetch from 'node-fetch'
+import { prisma } from '../../../lib/prisma.js'
+
+dotenv.config()
+
+const { AUTH_MERCADOPAGO } = process.env
+
+export const createOrder = async (id) => {
+  return id
+}
+
+export const updateOrder = async (id) => {
+  return id
+}
+
+export const cancelOrder = async (id) => {
+  return id
+}
