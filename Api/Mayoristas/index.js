2a2865437b315da1c4e6d4c75041b482b897741a
AgusIglesias72 <enpalabrass@gmail.com>
2023-06-22 15:24:21 -0300

    add mayoristas path

--- Api/Mayoristas/index.js
+++ Api/Mayoristas/index.js
@@ -1,18 +1,49 @@
 import express from 'express'
-import { createMayorista } from '../../src/Mayoristas/index.js'
+import {
+  createMayorista,
+  getMayoristas,
+  getOneMayorista,
+  updateMayorista,
+  deleteMayorista,
+} from '../../src/Mayoristas/index.js'
 
 const Mayoristas = express.Router()
 
 Mayoristas.get('/', async (req, res) => {
-  res.json({
-    message: 'Ruta aún no implementada',
-  })
+  const response = await getMayoristas()
+
+  return res.status(response.status).json(response)
 })
 
 Mayoristas.post('/', async (req, res) => {
   const { body } = req
 
   const response = await createMayorista(body)
+
+  return res.status(response.status).json(response)
+})
+
+Mayoristas.get('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const response = await getOneMayorista(id)
+
+  return res.status(response.status).json(response)
+})
+
+Mayoristas.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const { body } = req
+
+  const response = await updateMayorista(id, body)
+
+  return res.status(response.status).json(response)
+})
+
+Mayoristas.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const response = await deleteMayorista(id)
 
   return res.status(response.status).json(response)
 })
--- src/Mayoristas/index.js
+++ src/Mayoristas/index.js
@@ -60,3 +60,98 @@ export const createMayorista = async (body) => {
     }
   }
 }
+
+export const getMayoristas = async () => {
+  try {
+    const mayoristas = await prisma.mayoristas.findMany()
+
+    return {
+      error: false,
+      message: 'Se han encontrado los siguientes Revendedores',
+      status: 200,
+      data: mayoristas,
+    }
+  } catch (error) {
+    return {
+      error: true,
+      message: 'Error al buscar los Revendedores',
+      status: 500,
+      data: error,
+    }
+  }
+}
+
+export const getOneMayorista = async (id) => {
+  try {
+    const mayorista = await prisma.mayoristas.findUnique({
+      where: {
+        id: id,
+      },
+    })
+
+    return {
+      error: false,
+      message: 'Se ha encontrado el siguiente Revendedor',
+      status: 200,
+      data: mayorista,
+    }
+  } catch (error) {
+    return {
+      error: true,
+      message: 'Error al buscar el Revendedor',
+      status: 500,
+      data: error,
+    }
+  }
+}
+
+export const updateMayorista = async (id, body) => {
+  try {
+    const mayorista = await prisma.mayoristas.update({
+      where: {
+        id: id,
+      },
+      data: {
+        ...body,
+      },
+    })
+
+    return {
+      error: false,
+      message: 'Se ha actualizado correctamente el Revendedor',
+      status: 200,
+      data: mayorista,
+    }
+  } catch (error) {
+    return {
+      error: true,
+      message: 'Error al actualizar el Revendedor',
+      status: 500,
+      data: error,
+    }
+  }
+}
+
+export const deleteMayorista = async (id) => {
+  try {
+    const mayorista = await prisma.mayoristas.delete({
+      where: {
+        id: id,
+      },
+    })
+
+    return {
+      error: false,
+      message: 'Se ha eliminado correctamente el Revendedor',
+      status: 200,
+      data: mayorista,
+    }
+  } catch (error) {
+    return {
+      error: true,
+      message: 'Error al eliminar el Revendedor',
+      status: 500,
+      data: error,
+    }
+  }
+}
